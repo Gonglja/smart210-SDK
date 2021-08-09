6114fe4cb1aa4afda20d986367f5f39f0be9130f
gonglja <glj0@outlook.com>
2021-08-09 10:25:51 +0800

    Cortex-Debug

--- u-boot-2014.04/write2sd.sh
+++ u-boot-2014.04/write2sd.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-sudo dd iflag=sync oflag=sync if=spl/smart210-spl.bin of=/dev/sdc seek=1
-sudo dd iflag=sync oflag=sync if=u-boot.bin of=/dev/sdc seek=32
+DST=/dev/mmcblk0
+sudo dd iflag=sync oflag=sync if=spl/smart210-spl.bin of=$DST seek=1
+sudo dd iflag=sync oflag=sync if=u-boot.bin of=$DST  seek=32
 sync
 echo "done."
